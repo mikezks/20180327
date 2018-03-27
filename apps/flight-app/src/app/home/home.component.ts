e5fc49aec6a44f1e4db15878d694eb197a99096e
michael.zikes <cDs39wfT%24k>
2018-03-27 13:44:11 +0200

    RxJS

--- apps/flight-app/src/app/home/home.component.ts
+++ apps/flight-app/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
 import {ActivatedRoute} from '@angular/router';
+import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
+import { Subject } from "rxjs/Subject";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,9 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./home.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private route: ActivatedRoute) {
   }
@@ -17,10 +22,31 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.needsLogin = !!this.route.snapshot.params['needsLogin'];
+
+
+    let numbers = Observable.of(1,2,3);
+    numbers.subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+
+    let timer = Observable.timer(0, 2000)
+      //.take(5);
+      .takeUntil(this.destroy$);
+    this.subscription = timer.subscribe(
+      data => {
+        console.log(data);
+      }
+    );
   }
 
   get userName(): string {
     return this._userName;
+  }
+
+  destroyObs() {
+    this.destroy$.next(true);
   }
 
   login(): void {
@@ -29,6 +55,10 @@ export class HomeComponent implements OnInit {
 
   logout(): void {
     this._userName = '';
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
