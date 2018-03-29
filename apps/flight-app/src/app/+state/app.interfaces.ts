5efdd7bcb237375fae46865dad59186e8943e28d
michael.zikes <cDs39wfT%24k>
2018-03-29 23:50:04 +0200

    Router Store

--- apps/flight-app/src/app/+state/app.interfaces.ts
+++ apps/flight-app/src/app/+state/app.interfaces.ts
@@ -1,3 +1,5 @@
+import { RouterReducerState } from '@ngrx/router-store';
+import { RouterStateUrl } from './router-store.interfaces';
 
 export interface App {
   count: number;
@@ -5,4 +7,7 @@ export interface App {
 
 export interface AppState {
   readonly app: App;
+  readonly router: RouterReducerState<RouterStateUrl>;
 }
+
+
--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -2,6 +2,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  routerReducer,
+  RouterStateSerializer,
+  StoreRouterConnectingModule
+} from '@ngrx/router-store';
 import {FlightApiModule} from '@flight-workspace/flight-api';
 
 import {AppComponent} from './app.component';
@@ -13,14 +20,12 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {SharedModule} from './shared/shared.module';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import { EventService } from './event.service';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 import { appReducer } from './+state/app.reducer';
 import { appInitialState } from './+state/app.init';
 import { AppEffects } from './+state/app.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { CustomRouterStateSerializer } from './+state/router-store.interfaces';
 
 @NgModule({
   imports: [
@@ -30,7 +35,10 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
     FlightApiModule.forRoot(),
     SharedModule.forRoot(),
     RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS}),
-    StoreModule.forRoot({app: appReducer }, {initialState: {app: appInitialState}}),
+    StoreModule.forRoot(
+      { app: appReducer, router: routerReducer },
+      { initialState: { app: appInitialState, router: undefined } }
+    ),
     EffectsModule.forRoot([AppEffects]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreRouterConnectingModule
@@ -44,7 +52,8 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
   ],
   providers: [
     EventService,
-    AppEffects
+    AppEffects,
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
   ],
   bootstrap: [AppComponent]
 })
